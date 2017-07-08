68915d388147c59dff07e74dfb386bdf16376992
ernestrc <ernest@unstable.build>
2017-07-08 14:25:31 -0700

    moved config to separate file

---
+++ config.go
@@ -0,0 +1,26 @@
+package less
+
+import termbox "github.com/nsf/termbox-go"
+
+type Config struct {
+	tabspaces int
+	fg        termbox.Attribute
+	bg        termbox.Attribute
+	msgwidth  int8 // 0 - 100
+	wrap      bool
+	debug     bool
+	resfg     termbox.Attribute
+	resbg     termbox.Attribute
+	// TODO keyMap *KeyMap
+}
+
+var defaultConfig = Config{
+	tabspaces: 4,
+	fg:        termbox.ColorDefault,
+	bg:        termbox.ColorDefault,
+	msgwidth:  70,
+	wrap:      false,
+	debug:     false,
+	resfg:     termbox.AttrReverse,
+	resbg:     termbox.ColorDefault,
+}
--- less.go
+++ less.go
@@ -13,29 +13,6 @@ import (
 
 const bugMessage = "this is most likely a bug in the library. Please file bug at https://github.com/ernestrc/less/issues"
 
-type Config struct {
-	tabspaces int
-	fg        termbox.Attribute
-	bg        termbox.Attribute
-	msgwidth  int8 // 0 - 100
-	wrap      bool
-	debug     bool
-	resfg     termbox.Attribute
-	resbg     termbox.Attribute
-	// TODO keyMap *KeyMap
-}
-
-var defaultConfig = Config{
-	tabspaces: 4,
-	fg:        termbox.ColorDefault,
-	bg:        termbox.ColorDefault,
-	msgwidth:  70,
-	wrap:      false,
-	debug:     false,
-	resfg:     termbox.AttrReverse,
-	resbg:     termbox.ColorDefault,
-}
-
 type mode uint8
 
 const (
