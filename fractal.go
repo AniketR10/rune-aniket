b03d83a9f4ba17bc9b2083d77f8e0d44d2756eb4
ernestrc <ernest@unstable.build>
2019-09-19 13:17:44 -0700

    Add base types documentaion

--- fractal.go
+++ fractal.go
@@ -5,15 +5,39 @@ import (
 	"termbox"
 )
 
+// Component represents an element that can be drawn
+// in a text-based user interface. It wraps the basic Draw and Resize methods.
+//
+// Draw draws this component to the underlying Writer. It returns non-nil error
+// if something went wrong in the process of writing or the writer returned
+// an error.
+//
+// Resize is used by clients to indicate what's the virtual space available
+// for this component to be drawn in subsequent calls to Draw.
+// When a component is initialized, its width and height is 0 until Resize is
+// called to set the appropiate dimensions.
 type Component interface {
 	Resize(width, height int)
 	Draw(Writer) error
 }
 
+// Coordinates represent a point in a 2-D space.
 type Coordinates struct {
 	X, Y int
 }
 
+// Handler builds upon Component to add event-handling behavior.
+// It wraps the basic Handle, GetCursor and Man methods.
+//
+// Handle represents the ability to handle termbox events. These events could
+// be key presses or other types of events. See termbox' documentation for more
+// information. Handle returns true if a handler is done processing events.
+//
+// GetCursor returns a handler's cursor coordinates.
+// Clients can have multiple handlers in the same interface so
+// this method will be called only when handler is in focus.
+//
+// Man returns a Handler's usage manual. See Manual for more information.
 type Handler interface {
 	Component
 	Handle(termbox.Event) bool
@@ -21,20 +45,21 @@ type Handler interface {
 	Man() Manual
 }
 
+// Manual represents a handler's instruction manual and other
+// useful information to be displayed in a text-based user interface.
 type Manual struct {
 	Summary string
 	Keys    KeyMap
 }
 
+// KeyMap represents a Handler's key mapping information in the Manual.
 type KeyMap map[termbox.Event]struct {
 	ID          string
 	Description string
 }
 
-func Channel() <-chan termbox.Event {
-	return echan
-}
-
+// Init initializes this library. This function should be called before any
+// other functions. 'Close' must be called at the end to ensure graceful shutdown.
 func Init() error {
 	if err := termbox.Init(); err != nil {
 		return fmt.Errorf("failed termbox init: %v", err)
@@ -48,23 +73,31 @@ func Init() error {
 	return nil
 }
 
+// SetAttr sets the global foreground and background attributes.
 func SetAttr(fg, bg, highlightfg, highlightbg termbox.Attribute) {
 	foreground, background = fg, bg
 }
 
+// Run takes the given root handler, renders it full-screen,
+// and starts feeding it with termbox Events.
+// Error is non-nil if there were any errors.
 func Run(root Handler) (err error) {
 	return run(root, &termboxWriter{})
 }
 
+// RunMode runs the given root handler with the given termbox InputMode.
+// See Run for more information.
 func RunMode(root Handler, mode termbox.InputMode) (err error) {
 	termbox.SetInputMode(mode)
 	return Run(root)
 }
 
+// Size returns the total available width and height in the current terminal.
 func Size() (width int, height int) {
 	return termbox.Size()
 }
 
+// Close should be called when this library is not required anynmore.
 func Close() {
 	termbox.Close()
 }
