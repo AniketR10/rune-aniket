6395bba7954b00873e64989148a676a0ec32531f
ernestrc <ernest@unstable.build>
2020-11-01 13:07:02 -0800

    Add component.StringBackground

--- component/util.go
+++ component/util.go
@@ -6,19 +6,29 @@ import (
 	"github.com/ernestrc/go-tui/term"
 )
 
-// String converts a string into a static tui.Compontent.
-func String(str string) tui.Component {
+// StringBackground converts a string into a static tui.Compontent,
+// and uses c as the background rune.
+func StringBackground(str string, c rune) tui.Component {
 	var buf cell.Buffer
 	var scroll Scroll
 	buf.Init()
 	buf.WriteString(str)
 	scroll.InitWithBuffer(&buf)
 
-	background := term.Cell{Bg: scroll.Attributes.Bg, Fg: scroll.Attributes.Fg}
+	background := term.Cell{
+		Ch: c,
+		Bg: scroll.Attributes.Bg,
+		Fg: scroll.Attributes.Fg,
+	}
 	span := NewSpan(WithBackground(&scroll, background))
 
 	span.Padding.Vertical = -buf.Height()
 	span.Padding.Horizontal = -buf.Width()
 
 	return span
+}
+
+// String converts a string into a static tui.Compontent.
+func String(str string) tui.Component {
+	return StringBackground(str, 0)
 }
