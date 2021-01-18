308bfb638ea7fae0ef1ece80690f7b33366b9f52
ernestrc <ernest@unstable.build>
2021-01-17 19:54:52 -0800

    Add component.Grid

---
+++ component/grid.go
@@ -0,0 +1,52 @@
+package component
+
+import (
+	"github.com/ernestrc/go-tui"
+	"github.com/ernestrc/go-tui/term"
+)
+
+type grid struct {
+	width, height int
+	matrix        [][]tui.Component
+}
+
+// Grid renders matrix as an equaly sized grid of components.
+// Note that each []tui.Component in matrix is treated as a row.
+func Grid(matrix [][]tui.Component) tui.Component {
+	return &grid{matrix: matrix}
+}
+
+func (g *grid) Resize(width, height int) {
+	g.width, g.height = width, height
+}
+
+func (g *grid) Draw(w term.Writer) {
+	var v Virtual
+
+	if len(g.matrix) == 0 {
+		return
+	}
+
+	heightPerComp := g.height / len(g.matrix)
+	for y, row := range g.matrix {
+		yComp := y * heightPerComp
+		if yComp+heightPerComp > g.height {
+			break
+		}
+		if len(row) == 0 {
+			continue
+		}
+		widthPerComp := g.width / len(row)
+		for x, comp := range row {
+			xComp := x * widthPerComp
+			if xComp+widthPerComp > g.width {
+				break
+			}
+
+			v.C = comp
+			v.Resize(widthPerComp, heightPerComp)
+			v.Move(term.Coordinates{X: xComp, Y: yComp})
+			v.Draw(w)
+		}
+	}
+}
---
+++ component/grid_test.go
@@ -0,0 +1,107 @@
+package component
+
+import (
+	"testing"
+
+	"github.com/ernestrc/go-tui"
+	"github.com/ernestrc/go-tui/term"
+	testutil "github.com/ernestrc/go-tui/util/test"
+)
+
+func TestGridDraw(t *testing.T) {
+	t.Run("zero matrix", func(t *testing.T) {
+		l := Grid(nil)
+		l.Resize(4, 4)
+		w := term.NewStringWriter(20, 9)
+		tests := []testutil.ComponentTestCase{
+			{
+				nil, `
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    `,
+			},
+		}
+		testutil.TestComponent(t, l, w, tests)
+	})
+
+	t.Run("happy path", func(t *testing.T) {
+		l := Grid([][]tui.Component{
+			[]tui.Component{&TestComponent{Ch: 'a'}, &TestComponent{Ch: 'b'}},
+			[]tui.Component{&TestComponent{Ch: 'c'}, &TestComponent{Ch: 'd'}},
+			[]tui.Component{&TestComponent{Ch: 'e'}, &TestComponent{Ch: 'f'}},
+		})
+		l.Resize(20, 4)
+
+		w := term.NewStringWriter(20, 9)
+
+		tests := []testutil.ComponentTestCase{
+			{
+				nil, `
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+                    
+                    
+                    
+                    
+                    
+                    `,
+			}, {
+				func() { l.Resize(20, 9) }, `
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+ccccccccccdddddddddd
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+eeeeeeeeeeffffffffff
+eeeeeeeeeeffffffffff`,
+			}, {
+				func() { l.Resize(2, 2) }, `
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    `,
+			},
+		}
+		testutil.TestComponent(t, l, w, tests)
+	})
+
+	t.Run("zero row", func(t *testing.T) {
+		l := Grid([][]tui.Component{
+			[]tui.Component{&TestComponent{Ch: 'a'}},
+			nil,
+			[]tui.Component{&TestComponent{Ch: 'b'}},
+		})
+		l.Resize(4, 4)
+		w := term.NewStringWriter(20, 9)
+		tests := []testutil.ComponentTestCase{
+			{
+				nil, `
+aaaa                
+                    
+bbbb                
+                    
+                    
+                    
+                    
+                    
+                    `,
+			},
+		}
+		testutil.TestComponent(t, l, w, tests)
+	})
+
+}
