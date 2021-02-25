248be7d81ddbdfe95fc36f7a6cb31c23c775b696
ernestrc <ernest@unstable.build>
2021-02-24 16:41:43 -0800

    Update ServeEditorEventHandler to take extra perms

--- plugin/util/editor.go
+++ plugin/util/editor.go
@@ -22,7 +22,7 @@ type editorGrantee struct {
 	broker     proto.MuxBroker
 	ed         editor.Editor
 	handler    CommandEventHandler
-	newHandler func(editor.Editor, plugin.Config) (CommandEventHandler, error)
+	newHandler func(editor.Editor, []plugin.Grant, proto.MuxBroker, plugin.Config) (CommandEventHandler, error)
 	cmds       []string
 	pconfig    plugin.Config
 	err        error
@@ -37,7 +37,18 @@ func (t *editorGrantee) Connected(broker proto.MuxBroker, config plugin.Config) 
 	t.pconfig = config
 }
 
-func (t *editorGrantee) subscribeToEvents() error {
+func (t *editorGrantee) setNewHandler(grants []plugin.Grant) (CommandEventHandler, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	h, err := t.newHandler(t.ed, grants, t.broker, t.pconfig)
+	if err != nil {
+		return nil, err
+	}
+	t.handler = h
+	return h, nil
+}
+
+func (t *editorGrantee) subscribeToEvents(grants []plugin.Grant) error {
 	evs := []editor.EventType{
 		editor.EventTypeClose,
 		editor.EventTypeFlush,
@@ -45,7 +56,7 @@ func (t *editorGrantee) subscribeToEvents() error {
 		editor.EventTypeInsert,
 		editor.EventTypeDelete,
 	}
-	h, err := t.newHandler(t.ed, t.pconfig)
+	h, err := t.setNewHandler(grants)
 	if err != nil {
 		return err
 	}
@@ -74,7 +85,7 @@ func (t *editorGrantee) PermissionGranted(grants []plugin.Grant) {
 		case plugin.PermissionEditor:
 			t.ed, err = plugin.Editor(grant.Token, t.broker)
 			if err == nil {
-				err = t.subscribeToEvents()
+				err = t.subscribeToEvents(grants)
 			}
 		}
 		if err != nil {
@@ -107,14 +118,18 @@ func (t *editorGrantee) Health() error {
 
 // ServeEditorEventHandler calls fn to build a CommandEventHandler,
 // subsribes it to all editor events and registers it as the CommandHandler
-// of cmds.
+// of cmds. It also requests extraPerms, in addition to plugin.PermissionEditor.
+// All granted permissions are returned in the fn callback. If one of the
+// permissions is denied, the plugin will exit with an error.
 func ServeEditorEventHandler(
 	cmds []string,
-	fn func(editor.Editor, plugin.Config) (CommandEventHandler, error),
+	fn func(editor.Editor, []plugin.Grant, proto.MuxBroker, plugin.Config) (CommandEventHandler, error),
+	extraPerms ...plugin.Permission,
 ) {
 	perms := []plugin.Permission{
 		plugin.PermissionEditor,
 	}
+	perms = append(perms, extraPerms...)
 	s := &editorGrantee{cmds: cmds, newHandler: fn}
 	plugin.Serve(s, perms...)
 }
