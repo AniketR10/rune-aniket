5058ea7204470265766124152da5c1e1b14e2b5f
ernestrc <ernest@unstable.build>
2021-03-09 10:07:40 -0800

    Add component.Sync

---
+++ component/sync.go
@@ -0,0 +1,29 @@
+package component
+
+import (
+	"sync"
+	"github.com/ernestrc/go-tui"
+	"github.com/ernestrc/go-tui/term"
+)
+
+type csync struct {
+	mu sync.Locker
+	c tui.Component
+}
+
+// Sync wraps a tui.Component to provide access synchronization with mu.
+func Sync(mu sync.Locker, c tui.Component) tui.Component {
+	return csync{mu: mu, c: c}
+}
+
+func (s csync) Resize(width, height int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.c.Resize(width, height)
+}
+
+func (s csync) Draw(w term.Writer) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.c.Draw(w)
+}
