d09d9b0af6673aa79f679b187f602ebc490f4c6d
ernestrc <ernest@unstable.build>
2021-03-17 09:24:41 -0700

    Add handler.Sync to synchronize handler access

---
+++ handler/sync.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"sync"
+
+	"github.com/ernestrc/go-tui"
+	"github.com/ernestrc/go-tui/term"
+)
+
+type hsync struct {
+	mu sync.Locker
+	h  tui.Handler
+}
+
+// Sync wraps a tui.Handler to provide access synchronization with mu.
+func Sync(mu sync.Locker, h tui.Handler) tui.Handler {
+	return hsync{mu: mu, h: h}
+}
+
+func (s hsync) Resize(width, height int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.h.Resize(width, height)
+}
+
+func (s hsync) Draw(w term.Writer) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.h.Draw(w)
+}
+
+func (s hsync) Handle(ev term.Event) (exit, handled bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.h.Handle(ev)
+}
+
+func (s hsync) Cursor() (pos term.Coordinates, show bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.h.Cursor()
+}
+
+func (s hsync) Man() tui.Manual {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.h.Man()
+}
