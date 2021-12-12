d3ea90dac9d77dd997dac0b371cc34ced131a76e
ernestrc <ernest@unstable.build>
2021-12-12 14:14:20 -0800

    Add editor.Simple
    It's required for certain use cases. This implementation is not ready
    to be used as main editor. It's missing key features like mouse
    handling, delete selection and the like.

---
+++ editor/simple.go
@@ -0,0 +1,73 @@
+package editor
+
+import (
+	"errors"
+
+	"github.com/ernestrc/go-tui/cell"
+	"github.com/ernestrc/go-tui/term"
+)
+
+type simpleEditor struct {
+	pub Publisher
+}
+
+// Simple returns a simple to use Editor implementation.
+func Simple() Editor {
+	ret := new(simpleEditor)
+	ret.pub.Init()
+	return ret
+}
+
+func (e *simpleEditor) Edit(name string, buf *cell.Buffer) (Handler, error) {
+	root := newSimpleEditor(buf)
+	return e.pub.PublishEdit(name, buf, root, &root.cursor), nil
+}
+
+func (e *simpleEditor) SubscribeCommand(cmd string, h CommandHandler) error {
+	return errors.New("not supported")
+}
+
+func (e *simpleEditor) Editor(name string) (Handler, error) {
+	return nil, errors.New("not supported")
+}
+
+func (e *simpleEditor) SubscribeEditorEvents(ev EventType, sub EventHandler) error {
+	e.pub.SubscribeEditorEvents(ev, sub)
+	return nil
+}
+
+func (e simpleEditor) SetLocationList(h Handler, ID string, loc LocationList) error {
+	e.pub.Handler(h).(*simpleEditorHandler).cursor.SetLocationList(ID, loc)
+	return nil
+}
+
+func (e *simpleEditor) MoveToNextLocation(h Handler, ID string) error {
+	e.pub.Handler(h).(*simpleEditorHandler).cursor.MoveToNextLocation(ID)
+	return nil
+}
+
+func (e *simpleEditor) MoveToPrevLocation(h Handler, ID string) error {
+	e.pub.Handler(h).(*simpleEditorHandler).cursor.MoveToPrevLocation(ID)
+	return nil
+}
+
+func (e *simpleEditor) Reader(h Handler) Reader {
+	return CellReader(e.pub.Handler(h).(*simpleEditorHandler).buf.Reader())
+}
+
+func (e *simpleEditor) Writer(h Handler) Writer {
+	return CellWriter(e.pub.Handler(h).(*simpleEditorHandler).buf.Writer())
+}
+
+func (e *simpleEditor) SetCursor(h Handler, pos term.Coordinates) error {
+	_, ok := e.pub.Handler(h).(*simpleEditorHandler).cursor.MoveToScroll(pos)
+	if !ok {
+		return errors.New("MoveToScroll: invalid cursor position")
+	}
+	return nil
+}
+
+func (e *simpleEditor) Cursor(h Handler) (term.Coordinates, error) {
+	pos := e.pub.Handler(h).(*simpleEditorHandler).cursor.CursorAtScroll()
+	return pos, nil
+}
