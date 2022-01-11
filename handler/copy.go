a4c6b9245efe2f38d749460d822dc9596030c410
ernestrc <ernest@unstable.build>
2022-01-11 13:18:51 -0800

    Remove rpc breaker

---
+++ handler/copy.go
@@ -0,0 +1,21 @@
+package handler
+
+const (
+	loadingCopy   = "LOADING"
+	smtgWrongCopy = `
+
+          ___
+         /___/\_               
+        _\   \/_/\__           
+      __\       \/_/\          
+      \   __    __ \ \         
+     __\  \_\   \_\ \ \   __   
+    /_/\\   __   __  \ \_/_/\  
+    \_\/_\__\/\__\/\__\/_\_\/  
+       \_\/_/\       /_\_\/    
+          \_\/       \_\/      
+    
+
+Uh, Houston, we've had a problem
+`
+)
