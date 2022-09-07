e1721010f8d3fce1ae59aff6f1bd858201bfde6b
ernestrc <ernest@unstable.build>
2022-09-07 11:59:57 -0700

    Update debug logger to be goroutine-safe

--- debug/logger.go
+++ debug/logger.go
@@ -1,15 +1,23 @@
 package debug
 
 import (
+	"sync"
+
 	log "github.com/sirupsen/logrus"
 )
 
-var defaultLogger *log.Logger
+var (
+	defaultLogger *log.Logger
+	mu            sync.Mutex
+)
 
 // InitLogger initializes the default debug logger.
 // Note that this should never run production quality code.
 // Its purpose is to enable using a custom logger across packages.
 func InitLogger(logger *log.Logger) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	defaultLogger = logger
 }
 
@@ -17,6 +25,9 @@ func InitLogger(logger *log.Logger) {
 // Note that this should never run production quality code.
 // Its purpose is to enable using a custom logger across packages.
 func StandardLogger() *log.Logger {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if defaultLogger != nil {
 		return defaultLogger
 	}
