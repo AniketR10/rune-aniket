cf23aa7231f23bcf089382e49d54e5fc30c76fb6
ernestrc <ernest@unstable.build>
2022-10-11 11:42:06 -0700

    Add plugin_panic

---
+++ cmd/plugin_panic/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	_ "net/http/pprof"
+
+	"unstable.build/go-tui"
+	"unstable.build/go-tui/browser"
+	"unstable.build/go-tui/config"
+	"unstable.build/go-tui/plugin"
+	plugutil "unstable.build/go-tui/plugin/util"
+	"unstable.build/go-tui/proto"
+)
+
+func main() {
+	plugutil.ServeCommandSplitHandler(plugutil.CommandSplitHandlerConfig{
+		SplitOrientation: browser.OrientationRight,
+		Handler: func(grants []plugin.Grant, broker proto.MuxBroker,
+			invokeWindow browser.Window, config config.Config) (tui.Handler, error) {
+			return new(panicHandler), nil
+		},
+		Command: "panicPlugin",
+	})
+}
