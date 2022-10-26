1da1cd077dc1501a48651034dcc1c5af56637ec1
ernestrc <ernest@unstable.build>
2022-10-25 20:39:13 -0700

    Update term.proto to use bytes on raw field

--- term/rpc/convert.go
+++ term/rpc/convert.go
@@ -180,7 +180,7 @@ func (e *Event) ToModel() (ev term.Event, err error) {
 	ev.Ch = rune(e.Char)
 	ev.MouseX = int(e.GetMouseX())
 	ev.MouseY = int(e.GetMouseY())
-	ev.Raw = []byte(e.GetRaw())
+	ev.Raw = e.GetRaw()
 
 	return ev, nil
 }
@@ -410,7 +410,7 @@ func (e *Event) FromModel(ev term.Event) error {
 	e.Char = uint32(ev.Ch)
 	e.MouseX = int32(ev.MouseX)
 	e.MouseY = int32(ev.MouseY)
-	e.Raw = string(ev.Raw)
+	e.Raw = ev.Raw
 
 	return nil
 }
