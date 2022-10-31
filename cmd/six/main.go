63076d5a4b16b4f5fdb98674ca7a9090ef7a4a10
ernestrc <ernest@unstable.build>
2022-10-31 14:54:05 -0700

    Add signal handle to six workspace server

--- cmd/six/main.go
+++ cmd/six/main.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"os/signal"
 	"path"
 	"runtime"
 	"sync"
+	"syscall"
 
 	"github.com/ernestrc/blue/logging"
 	log "github.com/sirupsen/logrus"
@@ -62,6 +64,84 @@ func cwdURI() workspace.URI {
 	return uri
 }
 
+func startWorkspaceServer() {
+	l := log.New()
+
+	newScheme := workspace.NewFileScheme
+	exit := os.Exit
+	if serverLogs := *flagWorkspaceServerLogFile; serverLogs != "" {
+		f, err := os.OpenFile(serverLogs,
+			os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		l.SetOutput(f)
+		l.SetLevel(log.TraceLevel)
+		l.SetFormatter(&logging.LogrusFormatter{})
+
+		newScheme = workspace.LoggingScheme("file", newScheme)
+		exit = func(code int) {
+			_ = f.Sync()
+			_ = f.Close()
+			os.Exit(code)
+		}
+
+	} else {
+		l.SetOutput(ioutil.Discard)
+		l.SetLevel(log.PanicLevel)
+	}
+
+	l.Tracef("Initialized debug logger")
+
+	// log unhandled signals for debugging
+	ch := make(chan os.Signal, 1)
+	quitch := make(chan struct{})
+
+	defer close(quitch)
+	signal.Notify(ch)
+	go func() {
+		for {
+			select {
+			case sig := <-ch:
+				switch sig {
+				case syscall.SIGTERM, syscall.SIGKILL:
+					l.Info("Received kill signal: exiting")
+					exit(1)
+				case syscall.SIGURG:
+					/* received when socket urgent data is ready to be read */
+				default:
+					l.Debugf("Received unhandled signal: %#v", sig)
+				}
+			case <-quitch:
+				signal.Reset()
+				return
+			}
+		}
+	}()
+
+	debug.InitLogger(l)
+
+	uri, err := workspace.CurrentUserHostURI(*flagWorkspaceServer)
+	if err != nil {
+		l.Error(err)
+		exit(1)
+	}
+	scheme, err := newScheme(config.NopConfig(), uri)
+	if err != nil {
+		l.Error(err)
+		exit(2)
+	}
+
+	server := workspacepb.NewSchemeServer(scheme, new(sync.Mutex))
+	err = ssh.StartSchemeServer(server)
+	if err != nil {
+		l.Error(err)
+		exit(3)
+	}
+	l.Tracef("StartSchemeServer returned with no error")
+	exit(0)
+}
+
 func main() {
 	var err error
 	var filenames []string
@@ -88,55 +168,7 @@ func main() {
 	proto.DisableGRPCLogging()
 
 	if *flagWorkspaceServer != "" {
-		l := log.New()
-
-		newScheme := workspace.NewFileScheme
-		exit := os.Exit
-		if serverLogs := *flagWorkspaceServerLogFile; serverLogs != "" {
-			f, err := os.OpenFile(serverLogs,
-				os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-			if err != nil {
-				log.Fatal(err)
-			}
-			l.SetOutput(f)
-			l.SetLevel(log.TraceLevel)
-			l.SetFormatter(&logging.LogrusFormatter{})
-
-			newScheme = workspace.LoggingScheme("file", newScheme)
-			exit = func(code int) {
-				_ = f.Sync()
-				_ = f.Close()
-				os.Exit(code)
-			}
-
-		} else {
-			l.SetOutput(ioutil.Discard)
-			l.SetLevel(log.PanicLevel)
-		}
-
-		l.Tracef("Initialized debug logger")
-
-		debug.InitLogger(l)
-
-		uri, err := workspace.CurrentUserHostURI(*flagWorkspaceServer)
-		if err != nil {
-			l.Error(err)
-			exit(1)
-		}
-		scheme, err := newScheme(config.NopConfig(), uri)
-		if err != nil {
-			l.Error(err)
-			exit(2)
-		}
-
-		server := workspacepb.NewSchemeServer(scheme, new(sync.Mutex))
-		err = ssh.StartSchemeServer(server)
-		if err != nil {
-			l.Error(err)
-			exit(3)
-		}
-		l.Tracef("StartSchemeServer returned with no error")
-		exit(0)
+		startWorkspaceServer()
 	}
 
 	if *flagConfigPath != defaultConfigPath {
