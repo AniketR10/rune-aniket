74d38ed7417c1ba00004112e7ab749c624d272d4
ernestrc <ernest@unstable.build>
2023-03-14 21:09:45 -0700

    Remove clipboard gomock and proto go:generate

--- generate.go
+++ generate.go
@@ -3,5 +3,3 @@ package tui
 // TODO move gomocks to <package>/test sub-package
 //go:generate mockgen -destination=./proto/grpc_gomock.go -package proto google.golang.org/grpc ClientConnInterface
 //go:generate mockgen -destination=./proto/gomock_broker.go -package proto -self_package unstable.build/go-tui/proto -source ./proto/broker.go
-//go:generate mockgen -destination=./plugin/closer_gomock_test.go -package plugin -self_package unstable.build/go-tui/plugin -source ./plugin/clipboard_rpc_test.go
-//go:generate mockgen -destination=./plugin/clipboard_gomock_test.go -package plugin -self_package unstable.build/go-tui/plugin -source ./plugin/clipboard.go
--- plugin/generate.go
+++ plugin/generate.go
@@ -1,4 +1,3 @@
 package plugin
 
 //go:generate protoc rpc/grantee.proto -I=. -I=../ --go_out=./ --go-grpc_out=./ --go_opt=Mrpc/grantee.proto=unstable.build/go-tui/plugin/rpc
-//go:generate protoc rpc/clipboard.proto -I=. -I=../ --go_out=./ --go-grpc_out=./ --go_opt=Mrpc/clipboard.proto=unstable.build/go-tui/plugin/rpc
