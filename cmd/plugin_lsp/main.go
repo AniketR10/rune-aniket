1bce9614a29c486758a720bc07759e751c19d560
ernestrc <ernest@unstable.build>
2023-03-16 16:10:35 -0700

    Add pprof http handler to plugin_lsp

--- cmd/plugin_lsp/main.go
+++ cmd/plugin_lsp/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 
 	plugutil "unstable.build/go-tui/plugin/util"
 )
 
 func main() {
 	go func() {
-		log.Println(http.ListenAndServe("localhost:6063", nil))
+		log.Println(http.ListenAndServe("localhost:6033", nil))
 	}()
 
 	plugutil.ServeEditorEventHandler(lspHandlerCommands, newLspHandler,
