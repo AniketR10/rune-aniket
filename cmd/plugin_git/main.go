ec64648792728dbc572beb7b7b1691d6e1a1ea96
ernestrc <ernest@unstable.build>
2023-03-29 14:01:53 -0700

    Update plugin_git to expose pprof server

--- cmd/plugin_git/main.go
+++ cmd/plugin_git/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	plugutil "unstable.build/go-tui/plugin/util"
 )
 
 func main() {
-	/* go func() {
-		log.Println(http.ListenAndServe("localhost:6063", nil))
-	}()*/
+	go func() {
+		log.Println(http.ListenAndServe("localhost:3863", nil))
+	}()
 
 	plugutil.ServeEditorEventHandler(gitHandlerCommands, newGitHandler,
 		gitHandlerEvents, gitHandlerPermissions...)
