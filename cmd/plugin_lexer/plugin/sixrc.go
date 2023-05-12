f094a854a6c0687756d6b0ec140f2f0a1fc6710e
ernestrc <ernest@unstable.build>
2023-05-12 11:19:16 -0700

    Update plugin_lexer to treat .hoprc as a yaml file

--- cmd/plugin_lexer/plugin/sixrc.go
+++ cmd/plugin_lexer/plugin/sixrc.go
@@ -13,7 +13,7 @@ func setupConfigLexer() error {
 	}
 	cfg := lexer.Config()
 	cfg.Name = "sixrc"
-	cfg.Filenames = []string{".sixrc", ".sixdevrc"}
+	cfg.Filenames = []string{".sixrc", ".sixdevrc", ".hoprc", ".hopdevrc"}
 	lexers.Register(lexer)
 	return nil
 }
