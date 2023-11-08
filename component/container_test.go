8f2a61787f569b8ee0d7805b7a21cac529b498c4
ernestrc <ernest@unstable.build>
2023-11-08 10:41:04 -0800

    Add more tests to component.Container

--- component/container_test.go
+++ component/container_test.go
@@ -33,14 +33,15 @@ func TestContainerDraw(t *testing.T) {
 	t.Run("happy path", func(t *testing.T) {
 		l := NewContainer()
 		r1 := l.AddRow()
-		r1.AddComponent(testResponsive('a'), 6)
-		r1.AddComponent(testResponsive('b'), 6)
+		a := testResponsive('a', 1)
+		r1.AddComponent(a, 6)
+		r1.AddComponent(testResponsive('b', 1), 6)
 		r2 := l.AddRow()
-		r2.AddComponent(testResponsive('c'), 6)
-		r2.AddComponent(testResponsive('d'), 6)
+		r2.AddComponent(testResponsive('c', 1), 6)
+		r2.AddComponent(testResponsive('d', 1), 6)
 		r3 := l.AddRow()
-		r3.AddComponent(testResponsive('e'), 6)
-		r3.AddComponent(testResponsive('f'), 6)
+		r3.AddComponent(testResponsive('e', 1), 6)
+		r3.AddComponent(testResponsive('f', 1), 6)
 		l.Resize(20, 4)
 
 		w := term.NewStringWriter(20, 9)
@@ -129,14 +130,89 @@ ef
 					}
 					l.Resize(20, 9)
 					row := l.AddRow()
-					row.AddComponent(testResponsive('Z'), 3)
-					row.AddComponent(testResponsive('Y'), 6)
-					row.AddComponent(testResponsive('X'), 3)
+					row.AddComponent(testResponsive('Z', 1), 3)
+					row.AddComponent(testResponsive('Y', 1), 6)
+					row.AddComponent(testResponsive('X', 1), 3)
 				}, `
 aaaaaaaaaabbbbbbbbbb
 ccccccccccdddddddddd
 eeeeeeeeeeffffffffff
 ZZZZZYYYYYYYYYYXXXXX
+                    
+                    
+                    
+                    
+                    `,
+			}, {
+				func() {
+					row := l.AddRow()
+					row.AddComponent(testResponsive('3', 3), 3)
+					row.AddComponent(testResponsive('2', 2), 3)
+					row.AddComponent(testResponsive('1', 1), 3)
+				}, `
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+ZZZZZYYYYYYYYYYXXXXX
+333332222211111     
+333332222211111     
+333332222211111     
+                    
+                    `,
+			}, {
+				func() {
+					// changing height requirements without container
+					// or row "knowing" about it
+					a.(*TestResponsive).WantHeight = 2
+				}, `
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+ZZZZZYYYYYYYYYYXXXXX
+333332222211111     
+333332222211111     
+333332222211111     
+                    `,
+			}, {
+				func() {
+					a.(*TestResponsive).WantHeight = 4
+				}, `
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+ZZZZZYYYYYYYYYYXXXXX
+333332222211111     
+333332222211111     `,
+			}, {
+				func() {
+					require.True(t, l.ScrollDown())
+				}, `
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+aaaaaaaaaabbbbbbbbbb
+ccccccccccdddddddddd
+eeeeeeeeeeffffffffff
+ZZZZZYYYYYYYYYYXXXXX
+333332222211111     
+333332222211111     
+333332222211111     `,
+			}, {
+				func() {
+					for l.ScrollDown() {
+					}
+					row := l.AddRow()
+					row.AddComponent(testResponsive('@', 3), 6)
+					row.AddComponent(testResponsive('#', 2), 5)
+					row.AddComponent(testResponsive('$', 1), 5)
+				}, `
+333332222211111     
+@@@@@@@@@@########$$
+@@@@@@@@@@########$$
+@@@@@@@@@@########$$
                     
                     
                     
@@ -150,10 +226,10 @@ ZZZZZYYYYYYYYYYXXXXX
 	t.Run("zero row", func(t *testing.T) {
 		l := NewContainer()
 		r1 := l.AddRow()
-		r1.AddComponent(testResponsive('a'), 12)
+		r1.AddComponent(testResponsive('a', 1), 12)
 		_ = l.AddRow()
 		r3 := l.AddRow()
-		r3.AddComponent(testResponsive('b'), 12)
+		r3.AddComponent(testResponsive('b', 1), 12)
 		l.Resize(4, 4)
 		w := term.NewStringWriter(20, 9)
 		tests := []testutil.ComponentTestCase{
@@ -175,9 +251,9 @@ bbbb
 
 }
 
-func testResponsive(ch rune) Responsive {
+func testResponsive(ch rune, wantHeight int) Responsive {
 	return &TestResponsive{
-		WantHeight: 1,
+		WantHeight: wantHeight,
 		TestComponent: TestComponent{
 			Ch: ch,
 		},
