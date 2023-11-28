HigherOrderLogic/rn

Fix responsive list seek end on resize from 0,0

component/responsive.go
@@ -49,6 +49,11 @@ func Buffer(buf *cell.Buffer, cfg StringResponsiveConfig) Responsive {
 	return &respBuf{buf: buf, respStr: respStr{cfg: cfg}}
 }
 
+// NopResponsive returns a Responsive tui.Component that draws nothing.
+func NopResponsive() Responsive {
+	return Buffer(cell.NewBuffer(), StringConfig{})
+}
+
 type respStr struct {
 	cfg StringResponsiveConfig
 	in  [][]term.Cell

AniketR10/rune-aniket

component/responsive.go
@@ -49,6 +49,11 @@ func Buffer(buf *cell.Buffer, cfg StringResponsiveConfig) Responsive {
 	return &respBuf{buf: buf, respStr: respStr{cfg: cfg}}
 }
 
+// NopResponsive returns a Responsive tui.Component that draws nothing.
+func NopResponsive() Responsive {
+	return Buffer(cell.NewBuffer(), StringResponsiveConfig{})
+}
+
 type respStr struct {
 	cfg StringResponsiveConfig
 	in  [][]term.Cell