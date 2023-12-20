4c11538e760ee04df94527eacb641a8b0148666d
ernestrc <ernest@unstable.build>
2023-12-20 10:21:30 -0800

    Update extension_lexer to expose /debug/pprof on :8882

--- cmd/extension_lexer/main.go
+++ cmd/extension_lexer/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"unstable.build/go-tui/cmd/extension_lexer/extension"
 	"unstable.build/go-tui/extension/process"
 )
 
 func main() {
-	/* go func() {
-		log.Println(http.ListenAndServe("localhost:6063", nil))
-	}()*/
+	go func() {
+		log.Println(http.ListenAndServe("localhost:8882", nil))
+	}()
 
 	grantee, perms := extension.Grantee()
 	process.Serve(grantee, perms...)
