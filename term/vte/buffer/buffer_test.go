1516696ea8ba60068a9240b60eb19df814681fa4
ernestrc <ernest@unstable.build>
2024-02-26 10:03:50 -0800

    Add vte/buffer package

---
+++ term/vte/buffer/buffer_test.go
@@ -0,0 +1,584 @@
+package buffer
+
+import (
+	"testing"
+
+	"github.com/ernestrc/tcell/v3"
+	"github.com/rivo/uniseg"
+	"github.com/stretchr/testify/require"
+	"unstable.build/go-tui/cell"
+	"unstable.build/go-tui/term"
+	"unstable.build/go-tui/term/vte/parser"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewBuffer(t *testing.T) {
+	b := New()
+	assert.Equal(t, 1, b.Width())
+	assert.Equal(t, 1, b.Height())
+	assert.Equal(t, 0, b.TopMargin())
+	assert.Equal(t, 1, b.BottomMargin())
+	assert.Equal(t, 0, b.CursorPosition().X)
+	assert.Equal(t, 0, b.CursorPosition().Y)
+	require.NotNil(t, b.Cells.RawCells())
+	assert.NotNil(t, b.Cursor().Charsets)
+
+	assert.Equal(t, [][]term.Cell{
+		{{Ch: ' ', Width: 1}},
+	}, b.Cells.RawCells())
+}
+
+func TestResize(t *testing.T) {
+	t.Run("resize up", func(t *testing.T) {
+		b := New()
+		b.Resize(2, 2)
+		assert.Equal(t, 2, b.Width())
+		assert.Equal(t, 2, b.Height())
+		assert.Equal(t, 0, b.TopMargin())
+		assert.Equal(t, 2, b.BottomMargin())
+		require.NotNil(t, b.Cells.RawCells())
+
+		assert.Equal(t, [][]term.Cell{
+			{{Ch: ' ', Width: 1}, {Ch: ' ', Width: 1}},
+			{{Ch: ' ', Width: 1}, {Ch: ' ', Width: 1}},
+		}, b.Cells.RawCells())
+	})
+	t.Run("resize down", func(t *testing.T) {
+		b := New()
+		b.Resize(3, 3)
+		b.Resize(2, 2)
+		assert.Equal(t, 2, b.Width())
+		assert.Equal(t, 2, b.Height())
+		assert.Equal(t, 0, b.TopMargin())
+		assert.Equal(t, 2, b.BottomMargin())
+		require.NotNil(t, b.Cells.RawCells())
+
+		assert.Equal(t, [][]term.Cell{
+			{{Ch: ' ', Width: 1}, {Ch: ' ', Width: 1}},
+			{{Ch: ' ', Width: 1}, {Ch: ' ', Width: 1}},
+		}, b.Cells.RawCells())
+	})
+}
+
+func TestResetLines(t *testing.T) {
+	t.Run("all", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb")
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+
+		b.ResetLines(0, 5)
+		assertEqualBuf(t, b, "     \n     \n     \n     \n     ")
+	})
+	t.Run("subset of lines at start", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb")
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+
+		b.ResetLines(0, 1)
+		assertEqualBuf(t, b, "     \nb    \n     \n     \n     ")
+	})
+	t.Run("subset of lines in the middle", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb")
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+
+		b.ResetLines(1, 2)
+		assertEqualBuf(t, b, "a    \n     \n     \n     \n     ")
+	})
+	t.Run("subset of lines at the end", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(4, 5)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \n     ")
+	})
+	t.Run("past last line does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(5, 6)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+	})
+	t.Run("start > end does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(3, 2)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+	})
+	t.Run("start == end does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(0, 0)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+	})
+	t.Run("negative start does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(-1, 2)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+	})
+	t.Run("negative end does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb\nc\nd\ne")
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+
+		b.ResetLines(1, -2)
+		assertEqualBuf(t, b, "a    \nb    \nc    \nd    \ne    ")
+	})
+}
+
+func TestResetCells(t *testing.T) {
+	t.Run("reset after content does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb")
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+
+		b.ResetCells(1, 5)
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+	})
+	t.Run("reset one", func(t *testing.T) {
+		b := makeBufferForTesting(5, 5)
+		writeToBuffer(b, "a\nb")
+		assertEqualBuf(t, b, "a    \nb    \n     \n     \n     ")
+
+		b.ResetCells(0, 1)
+		assertEqualBuf(t, b, "a    \n     \n     \n     \n     ")
+	})
+}
+
+func TestScrollUp(t *testing.T) {
+	t.Run("count zero does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 10, 0)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("all once", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 10, 1)
+		assertEqualBuf(t, b, "1\n2\n3\n4\n5\n6\n7\n8\n9\n ")
+	})
+	t.Run("all multi ", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 10, 2)
+		assertEqualBuf(t, b, "2\n3\n4\n5\n6\n7\n8\n9\n \n ")
+	})
+	t.Run("all exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 10, 10)
+		assertEqualBuf(t, b, " \n \n \n \n \n \n \n \n \n ")
+	})
+	t.Run("all more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 10, 11)
+		assertEqualBuf(t, b, " \n \n \n \n \n \n \n \n \n ")
+	})
+
+	t.Run("top once ", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 3, 1)
+		assertEqualBuf(t, b, "1\n2\n \n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 3, 2)
+		assertEqualBuf(t, b, "2\n \n \n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 3, 3)
+		assertEqualBuf(t, b, " \n \n \n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(0, 3, 4)
+		assertEqualBuf(t, b, " \n \n \n3\n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("mid once ", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(1, 4, 1)
+		assertEqualBuf(t, b, "0\n2\n3\n \n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(1, 4, 2)
+		assertEqualBuf(t, b, "0\n3\n \n \n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(1, 4, 3)
+		assertEqualBuf(t, b, "0\n \n \n \n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(1, 4, 4)
+		assertEqualBuf(t, b, "0\n \n \n \n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("bottom once ", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(7, 10, 1)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n8\n9\n ")
+	})
+	t.Run("bottom multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(7, 10, 2)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n9\n \n ")
+	})
+	t.Run("bottom exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(7, 10, 3)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n \n ")
+	})
+	t.Run("bottom more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollUp(7, 10, 4)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n \n ")
+	})
+
+	t.Run("once all but top line", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "a\nb\n \n \n \n \n \n \n \n ")
+		b.ScrollUp(1, 10, 1)
+		assertEqualBuf(t, b, "a\n \n \n \n \n \n \n \n \n ")
+	})
+}
+
+func TestScrollDown(t *testing.T) {
+	t.Run("count zero does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 10, 0)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("all once", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 10, 1)
+		assertEqualBuf(t, b, " \n0\n1\n2\n3\n4\n5\n6\n7\n8")
+	})
+	t.Run("all multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 10, 2)
+		assertEqualBuf(t, b, " \n \n0\n1\n2\n3\n4\n5\n6\n7")
+	})
+	t.Run("all exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 10, 10)
+		assertEqualBuf(t, b, " \n \n \n \n \n \n \n \n \n ")
+	})
+	t.Run("all more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 10, 11)
+		assertEqualBuf(t, b, " \n \n \n \n \n \n \n \n \n ")
+	})
+
+	t.Run("top once", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 3, 1)
+		assertEqualBuf(t, b, " \n0\n1\n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 3, 2)
+		assertEqualBuf(t, b, " \n \n0\n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 3, 3)
+		assertEqualBuf(t, b, " \n \n \n3\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("top more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(0, 3, 4)
+		assertEqualBuf(t, b, " \n \n \n3\n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("mid once", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(1, 4, 1)
+		assertEqualBuf(t, b, "0\n \n1\n2\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(1, 4, 2)
+		assertEqualBuf(t, b, "0\n \n \n1\n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(1, 4, 3)
+		assertEqualBuf(t, b, "0\n \n \n \n4\n5\n6\n7\n8\n9")
+	})
+	t.Run("mid more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(1, 4, 4)
+		assertEqualBuf(t, b, "0\n \n \n \n4\n5\n6\n7\n8\n9")
+	})
+
+	t.Run("bottom once ", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(7, 10, 1)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n7\n8")
+	})
+	t.Run("bottom multi", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(7, 10, 2)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n \n7")
+	})
+	t.Run("bottom exactly length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(7, 10, 3)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n \n ")
+	})
+	t.Run("bottom more than length", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9")
+		b.ScrollDown(7, 10, 4)
+		assertEqualBuf(t, b, "0\n1\n2\n3\n4\n5\n6\n \n \n ")
+	})
+	t.Run("once all but top line", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "a\nb\n \n \n \n \n \n \n \n ")
+		b.ScrollDown(1, 10, 1)
+		assertEqualBuf(t, b, "a\n \nb\n \n \n \n \n \n \n ")
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("deletes one character", func(t *testing.T) {
+		b := makeBufferForTesting(1, 10)
+		resetBuffer(t, b, "a\nb\n \n \n \n \n \n \n \n ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.Delete(1)
+		assertEqualBuf(t, b, " \nb\n \n \n \n \n \n \n \n ")
+	})
+
+	t.Run("deletes partial start of row", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.Delete(1)
+		assertEqualBuf(t, b, "a \nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("deletes partial end of row", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{X: 1}, false)
+		b.Delete(1)
+		assertEqualBuf(t, b, "a \nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("deletes multiple characters", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.Delete(2)
+		assertEqualBuf(t, b, "  \nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("past last column does nothing past last column", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.Delete(3)
+		assertEqualBuf(t, b, "  \nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("zero count does nothing", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.Delete(0)
+		assertEqualBuf(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+}
+
+func TestWriteInsert(t *testing.T) {
+	t.Run("maps character to ' ' if SetHiddenCursor(true) was called", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+		b.SetHiddenCursor(true)
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, " a\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetHiddenCursor(false)
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, "Xa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("copies the attributes last set via SetCursorAttributes", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+
+		attrs := term.Attributes{
+			Fg:    tcell.ColorYellow,
+			Bg:    tcell.ColorBlue,
+			Attrs: tcell.AttrItalic,
+		}
+		b.SetCursorAttributes(attrs)
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, "Xa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+
+		cell := b.Cells.RawCells()[0][0]
+		assert.Equal(t, attrs, cell.Attributes)
+	})
+
+	t.Run("maps the character using the given charset index", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+
+		b.ConfigureCharset(parser.CharsetIndexG1, parser.StandardCharsetSpecialCharacterAndLineDrawing)
+		b.Write('`', 1, parser.CharsetIndexG1)
+		assertEqualBuf(t, b, "◆a\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("does nothing if passed charset has not been configured", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{}, false)
+
+		b.Write('`', 1, parser.CharsetIndexG1)
+		assertEqualBuf(t, b, "`a\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("does not write character if out of bounds", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{X: 4}, false)
+
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("does not insert character if writing out of bounds (x)", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{X: 4}, false)
+
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("inserts character if writing out of bounds (y)", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{Y: 12, X: 6}, false)
+
+		b.Write('X', 1, 0)
+		assertEqualBuf(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("inserts character in bounds", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+		b.SetCursorPosition(term.Coordinates{X: 1}, false)
+
+		b.Insert('X', 1, 0)
+		assertEqualBuf(t, b, "aX\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+	})
+
+	t.Run("insert at pos guarantees that CellAt does not return nil", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+
+		b.SetCursorPosition(term.Coordinates{Y: 10}, false)
+		// invalidate cursor position
+		b.Resize(2, 9)
+
+		require.NotPanics(t, func() {
+			b.Insert('X', 1, 0)
+		})
+		assertEqualBuf(t, b, "  \n  \n  \n  \n  \n  \n  \n  \n  \nX")
+	})
+
+	t.Run("write at pos guarantees that CellAt does not return nil", func(t *testing.T) {
+		b := makeBufferForTesting(2, 10)
+		resetBuffer(t, b, "aa\nbb\n  \n  \n  \n  \n  \n  \n  \n  ")
+
+		b.SetCursorPosition(term.Coordinates{Y: 10}, false)
+		// invalidate cursor position
+		b.Resize(2, 9)
+
+		require.NotPanics(t, func() {
+			b.Write('X', 1, 0)
+		})
+		assertEqualBuf(t, b, "  \n  \n  \n  \n  \n  \n  \n  \n  \nX")
+	})
+}
+
+func makeBufferForTesting(width, height int) *Buffer {
+	ret := New()
+	ret.Resize(width, height)
+	return ret
+}
+
+func writeToBuffer(b *Buffer, str string) {
+	for _, ch := range str {
+		pos := b.CursorPosition()
+		if ch == '\n' {
+			pos.Y++
+			pos.X = 0
+			b.SetCursorPosition(pos, false)
+		} else {
+			b.Write(ch, uniseg.StringWidth(string(ch)), 0)
+			pos.X++
+			b.SetCursorPosition(pos, false)
+		}
+	}
+}
+
+func assertEqualBuf(t *testing.T, p *Buffer, expected string) {
+	assert.Equal(t, expected, cell.CellsToString(p.Cells.RawCells()))
+}
+
+func resetBuffer(t *testing.T, b *Buffer, to string) {
+	b.ResetLines(0, b.Height())
+	b.SetCursorPosition(term.Coordinates{}, false)
+	writeToBuffer(b, to)
+	require.Equal(t, to, cell.CellsToString(b.Cells.RawCells()))
+}
