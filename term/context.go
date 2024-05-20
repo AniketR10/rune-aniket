48ac498405cc2a087055fda225cc0c1625cc8849
ernestrc <ernest@unstable.build>
2024-05-20 16:28:26 -0700

    Fix term/context.go documentation

--- term/context.go
+++ term/context.go
@@ -8,7 +8,7 @@ import (
 // This prevents collisions with keys defined in other packages.
 type ctxKey int
 
-// idKey is the key for sync.Payload values in Contexts. It is
+// lockerKey is the key for sync.Payload values in Contexts. It is
 // unexported; clients use workspace.ContextWithPayload and
 // PayloadFromContext instead of using this key directly.
 var lockerKey ctxKey
@@ -18,7 +18,7 @@ func ContextWithPayload(ctx context.Context, payload []byte) context.Context {
 	return context.WithValue(ctx, lockerKey, payload)
 }
 
-// PayloadFromContext returns the ID value stored in ctx, if any.
+// PayloadFromContext returns the payload value stored in ctx, if any.
 func PayloadFromContext(ctx context.Context) ([]byte, bool) {
 	locker, ok := ctx.Value(lockerKey).([]byte)
 	return locker, ok
