54c84fc123c95525017bb815911f2f631deb31b7
ramon <ramon@unstable.build>
2024-07-30 10:24:16 +0000

    Add time shader: Sine
    Reviewed-on: https://git.unstable.build/unstablebuild/go-tui/pulls/34
    Reviewed-by: Ernest Romero Climent <ernest@unstable.build>
    Co-authored-by: ramon <ramon@unstable.build>
    Co-committed-by: ramon <ramon@unstable.build>

---
+++ component/shader/timeshader/sine.go
@@ -0,0 +1,53 @@
+// Unstable Build LLC ("COMPANY") CONFIDENTIAL
+//
+// Unpublished Copyright (c) 2017-2024 Unstable Build, All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of COMPANY.
+// The intellectual and technical concepts contained herein are proprietary to
+// COMPANY and may be covered by U.S. and Foreign Patents, patents in process,
+// and are protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written permission
+// is obtained from COMPANY. Access to the source code contained herein is hereby
+// forbidden to anyone except current COMPANY employees, managers or contractors who
+// have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+//
+// The copyright notice above does not evidence any actual or intended publication or
+// disclosure of this source code, which includes information that is confidential and/or
+// proprietary, and is a trade secret, of COMPANY. ANY REPRODUCTION, MODIFICATION,
+// DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE
+// WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN
+// VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+// THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+// REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+// ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+package timeshader
+
+import (
+	"math"
+
+	"unstable.build/go-tui/component/shader"
+)
+
+// Sine creates a full sine wave cycle (start .. end .. start .. -end .. start)
+// or the corresponding portion of it (or multiple of them) specified by the
+// "revolutions" parameter.
+func Sine(baseShader shader.Shader, revolutions float64) shader.Shader {
+	return sine(baseShader, revolutions)
+}
+
+// Boomerang creates a half sine wave cycle (start .. end .. start) creating a
+// curved time animation playing twice the speed til the last frame and coming
+// back to initial frame.
+func Boomerang(baseShader shader.Shader, revolutions float64) shader.Shader {
+	return sine(baseShader, 0.5)
+}
+
+func sine(baseShader shader.Shader, revolutions float64) *timeShader {
+	return &timeShader{baseShader, funcTimeRemapper(
+		func(frame, total int) int {
+			return int(math.Round(float64(total-1) *
+				math.Sin(revolutions*2*math.Pi*float64(frame)/float64(total-1))),
+			)
+		},
+	)}
+}
