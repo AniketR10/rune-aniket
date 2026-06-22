8d7d2e7585b0cefb2928fa290c3c474e5ec5c2a2
ernestrc <ernest@unstable.build>
2026-06-22 16:48:44 +0200

    Update file explorer to maintain expanded nodes upon refresh

--- ide/ex.go
+++ ide/ex.go
@@ -1908,6 +1908,7 @@ func (e *ex) initFileExplorer() error {
 	} else {
 		// Keep the target in sync with the latest focus.
 		e.fileExplorerHandler.SetTargetWindow(e.fileExplorerTarget)
+		e.fileExplorerHandler.refreshTree()
 	}
 
 	win, err := e.comp.SplitRoot(component.AlignmentLeft, e.fileExplorerHandler)
